45d2a7fe8d08d47dab8d2b6110bb253fd42565f7
onorio <onorio@catalpa.io>
2024-04-13 10:18:02 +0900

    change code kmk to kem

--- tests/test_kem.py
+++ tests/test_kem.py
@@ -30,261 +30,261 @@ class Num2WordsKEMTest(TestCase):
         self.n2w = Num2Word_KEM()
 
     def test_cardinal_integer(self):
-        self.assertEqual(num2words(1, lang='kmk'), 'sia')
-        self.assertEqual(num2words(2, lang='kmk'), 'rua')
-        self.assertEqual(num2words(3, lang='kmk'), 'telu')
-        self.assertEqual(num2words(4, lang='kmk'), 'paat')
-        self.assertEqual(num2words(5, lang='kmk'), 'lima')
-        self.assertEqual(num2words(6, lang='kmk'), 'neem')
-        self.assertEqual(num2words(7, lang='kmk'), 'itsu')
-        self.assertEqual(num2words(8, lang='kmk'), 'balu')
-        self.assertEqual(num2words(9, lang='kmk'), 'sibe')
-        self.assertEqual(num2words(10, lang='kmk'), 'sapulu')
-        self.assertEqual(num2words(11, lang='kmk'), 'sapulu resin sia')
-        self.assertEqual(num2words(12, lang='kmk'), 'sapulu resin rua')
-        self.assertEqual(num2words(13, lang='kmk'), 'sapulu resin telu')
-        self.assertEqual(num2words(14, lang='kmk'), 'sapulu resin paat')
-        self.assertEqual(num2words(15, lang='kmk'), 'sapulu resin lima')
-        self.assertEqual(num2words(16, lang='kmk'), 'sapulu resin neem')
-        self.assertEqual(num2words(17, lang='kmk'), 'sapulu resin itsu')
-        self.assertEqual(num2words(18, lang='kmk'), 'sapulu resin balu')
-        self.assertEqual(num2words(19, lang='kmk'), 'sapulu resin sibe')
-        self.assertEqual(num2words(20, lang='kmk'), 'gulurua')
+        self.assertEqual(num2words(1, lang='kem'), 'sia')
+        self.assertEqual(num2words(2, lang='kem'), 'rua')
+        self.assertEqual(num2words(3, lang='kem'), 'telu')
+        self.assertEqual(num2words(4, lang='kem'), 'paat')
+        self.assertEqual(num2words(5, lang='kem'), 'lima')
+        self.assertEqual(num2words(6, lang='kem'), 'neem')
+        self.assertEqual(num2words(7, lang='kem'), 'itsu')
+        self.assertEqual(num2words(8, lang='kem'), 'balu')
+        self.assertEqual(num2words(9, lang='kem'), 'sibe')
+        self.assertEqual(num2words(10, lang='kem'), 'sapulu')
+        self.assertEqual(num2words(11, lang='kem'), 'sapulu resin sia')
+        self.assertEqual(num2words(12, lang='kem'), 'sapulu resin rua')
+        self.assertEqual(num2words(13, lang='kem'), 'sapulu resin telu')
+        self.assertEqual(num2words(14, lang='kem'), 'sapulu resin paat')
+        self.assertEqual(num2words(15, lang='kem'), 'sapulu resin lima')
+        self.assertEqual(num2words(16, lang='kem'), 'sapulu resin neem')
+        self.assertEqual(num2words(17, lang='kem'), 'sapulu resin itsu')
+        self.assertEqual(num2words(18, lang='kem'), 'sapulu resin balu')
+        self.assertEqual(num2words(19, lang='kem'), 'sapulu resin sibe')
+        self.assertEqual(num2words(20, lang='kem'), 'gulurua')
 
-        self.assertEqual(num2words(21, lang='kmk'), 'gulurua resin sia')
-        self.assertEqual(num2words(22, lang='kmk'), 'gulurua resin rua')
-        self.assertEqual(num2words(35, lang='kmk'), 'gulutelu resin lima')
-        self.assertEqual(num2words(99, lang='kmk'), 'gulusibe resin sibe')
+        self.assertEqual(num2words(21, lang='kem'), 'gulurua resin sia')
+        self.assertEqual(num2words(22, lang='kem'), 'gulurua resin rua')
+        self.assertEqual(num2words(35, lang='kem'), 'gulutelu resin lima')
+        self.assertEqual(num2words(99, lang='kem'), 'gulusibe resin sibe')
 
-        self.assertEqual(num2words(100, lang='kmk'), 'atsus sia')
-        self.assertEqual(num2words(101, lang='kmk'), 'atsus sia resin sia')
-        self.assertEqual(num2words(107, lang='kmk'), 'atsus sia resin itsu')
-        self.assertEqual(num2words(110, lang='kmk'), 'atsus sia sapulu')
-        self.assertEqual(num2words(114, lang='kmk'), 'atsus sia sapulu resin paat')
-        self.assertEqual(num2words(128, lang='kmk'), 'atsus sia gulurua resin balu')
-        self.assertEqual(num2words(151, lang='kmk'), 'atsus sia gululima resin sia')
-        self.assertEqual(num2words(713, lang='kmk'), 'atsus itsu sapulu resin telu')
-        self.assertEqual(num2words(999, lang='kmk'), 'atsus sibe gulusibe resin sibe')
+        self.assertEqual(num2words(100, lang='kem'), 'atsus sia')
+        self.assertEqual(num2words(101, lang='kem'), 'atsus sia resin sia')
+        self.assertEqual(num2words(107, lang='kem'), 'atsus sia resin itsu')
+        self.assertEqual(num2words(110, lang='kem'), 'atsus sia sapulu')
+        self.assertEqual(num2words(114, lang='kem'), 'atsus sia sapulu resin paat')
+        self.assertEqual(num2words(128, lang='kem'), 'atsus sia gulurua resin balu')
+        self.assertEqual(num2words(151, lang='kem'), 'atsus sia gululima resin sia')
+        self.assertEqual(num2words(713, lang='kem'), 'atsus itsu sapulu resin telu')
+        self.assertEqual(num2words(999, lang='kem'), 'atsus sibe gulusibe resin sibe')
 
-        self.assertEqual(num2words(1000, lang='kmk'), 'ribun sia')
-        self.assertEqual(num2words(1001, lang='kmk'), 'ribun sia resin sia')
-        self.assertEqual(num2words(1011, lang='kmk'), 'ribun sia sapulu resin sia')
-        self.assertEqual(num2words(1111, lang='kmk'), 'ribun sia atsus sia sapulu resin sia')
-        self.assertEqual(num2words(2357, lang='kmk'), 'ribun rua atsus telu gululima resin itsu')
+        self.assertEqual(num2words(1000, lang='kem'), 'ribun sia')
+        self.assertEqual(num2words(1001, lang='kem'), 'ribun sia resin sia')
+        self.assertEqual(num2words(1011, lang='kem'), 'ribun sia sapulu resin sia')
+        self.assertEqual(num2words(1111, lang='kem'), 'ribun sia atsus sia sapulu resin sia')
+        self.assertEqual(num2words(2357, lang='kem'), 'ribun rua atsus telu gululima resin itsu')
         self.assertEqual(
-            num2words(2200, lang='kmk'),
+            num2words(2200, lang='kem'),
             'ribun rua atsus rua'
         )
-        self.assertEqual(num2words(2230, lang='kmk'), 'ribun rua atsus rua gulutelu')
-        self.assertEqual(num2words(73400, lang='kmk'), 'ribun guluitsu resin telu atsus paat')
-        self.assertEqual(num2words(73421, lang='kmk'), 'ribun guluitsu resin telu atsus paat gulurua resin sia')
-        self.assertEqual(num2words(100000, lang='kmk'), 'ribun atsus sia')
-        self.assertEqual(num2words(250050, lang='kmk'), 'ribun atsus rua gululima gululima')
+        self.assertEqual(num2words(2230, lang='kem'), 'ribun rua atsus rua gulutelu')
+        self.assertEqual(num2words(73400, lang='kem'), 'ribun guluitsu resin telu atsus paat')
+        self.assertEqual(num2words(73421, lang='kem'), 'ribun guluitsu resin telu atsus paat gulurua resin sia')
+        self.assertEqual(num2words(100000, lang='kem'), 'ribun atsus sia')
+        self.assertEqual(num2words(250050, lang='kem'), 'ribun atsus rua gululima gululima')
         self.assertEqual(
-            num2words(6000000, lang='kmk'), 'miliaun neem'
+            num2words(6000000, lang='kem'), 'miliaun neem'
         )
         self.assertEqual(
-            num2words(100000000, lang='kmk'), 'miliaun atsus sia'
+            num2words(100000000, lang='kem'), 'miliaun atsus sia'
         )
         self.assertEqual(
-            num2words(19000000000, lang='kmk'), 'miliaun ribun sapulu resin sibe'
+            num2words(19000000000, lang='kem'), 'miliaun ribun sapulu resin sibe'
         )
         self.assertEqual(
-            num2words(145000000002, lang='kmk'),
+            num2words(145000000002, lang='kem'),
             'miliaun ribun atsus sia gulupaat resin lima resin rua'
         )
         self.assertEqual(
-            num2words(4635102, lang='kmk'),
+            num2words(4635102, lang='kem'),
             'miliaun paat ribun atsus neem gulutelu resin lima atsus sia resin rua'
         )
         self.assertEqual(
-            num2words(145254635102, lang='kmk'),
+            num2words(145254635102, lang='kem'),
             'miliaun ribun atsus sia gulupaat resin lima atsus rua gululima resin paat ribun atsus neem gulutelu resin lima atsus sia resin rua'
         )
         self.assertEqual(
-            num2words(1000000000000, lang='kmk'),
+            num2words(1000000000000, lang='kem'),
             'biliaun sia'
         )
         self.assertEqual(
-            num2words(2000000000000, lang='kmk'),
+            num2words(2000000000000, lang='kem'),
             'biliaun rua'
         )
         self.assertEqual(
-            num2words(1000000000000000, lang='kmk'),
+            num2words(1000000000000000, lang='kem'),
             'biliaun ribun sia'
         )
         self.assertEqual(
-            num2words(2000000000000000, lang='kmk'),
+            num2words(2000000000000000, lang='kem'),
             'biliaun ribun rua'
         )
         self.assertEqual(
-            num2words(1000000000000000000, lang='kmk'),
+            num2words(1000000000000000000, lang='kem'),
             'triliaun sia'
         )
         self.assertEqual(
-            num2words(2000000000000000000, lang='kmk'),
+            num2words(2000000000000000000, lang='kem'),
             'triliaun rua'
         )
 
     def test_cardinal_integer_negative(self):
-        self.assertEqual(num2words(-1, lang='kmk'), 'menus sia')
+        self.assertEqual(num2words(-1, lang='kem'), 'menus sia')
         self.assertEqual(
-            num2words(-256, lang='kmk'), 'menus atsus rua gululima resin neem'
+            num2words(-256, lang='kem'), 'menus atsus rua gululima resin neem'
         )
-        self.assertEqual(num2words(-1000, lang='kmk'), 'menus ribun sia')
-        self.assertEqual(num2words(-1000000, lang='kmk'), 'menus miliaun sia')
+        self.assertEqual(num2words(-1000, lang='kem'), 'menus ribun sia')
+        self.assertEqual(num2words(-1000000, lang='kem'), 'menus miliaun sia')
         self.assertEqual(
-            num2words(-1234567, lang='kmk'),
+            num2words(-1234567, lang='kem'),
             'menus miliaun sia ribun atsus rua gulutelu resin paat atsus lima guluneem resin itsu'
         )
 
     def test_cardinal_float(self):
-        self.assertEqual(num2words(Decimal('1.00'), lang='kmk'), 'sia')
+        self.assertEqual(num2words(Decimal('1.00'), lang='kem'), 'sia')
         self.assertEqual(num2words(
-            Decimal('1.01'), lang='kmk'), 'sia vírgula bai sia')
+            Decimal('1.01'), lang='kem'), 'sia vírgula bai sia')
         self.assertEqual(num2words(
-            Decimal('1.035'), lang='kmk'), 'sia vírgula bai telu lima'
+            Decimal('1.035'), lang='kem'), 'sia vírgula bai telu lima'
         )
         self.assertEqual(num2words(
-            Decimal('1.35'), lang='kmk'), 'sia vírgula telu lima'
+            Decimal('1.35'), lang='kem'), 'sia vírgula telu lima'
         )
         self.assertEqual(
-            num2words(Decimal('3.14159'), lang='kmk'),
+            num2words(Decimal('3.14159'), lang='kem'),
             'telu vírgula sia paat sia lima sibe'
         )
         self.assertEqual(
-            num2words(Decimal('101.22'), lang='kmk'),
+            num2words(Decimal('101.22'), lang='kem'),
             'atsus sia resin sia vírgula rua rua'
         )
         self.assertEqual(
-            num2words(Decimal('2345.75'), lang='kmk'),
+            num2words(Decimal('2345.75'), lang='kem'),
             'ribun rua atsus telu gulupaat resin lima vírgula itsu lima'
         )
 
     def test_cardinal_float_negative(self):
         self.assertEqual(
-            num2words(Decimal('-2.34'), lang='kmk'),
+            num2words(Decimal('-2.34'), lang='kem'),
             'menus rua vírgula telu paat'
         )
         self.assertEqual(
-            num2words(Decimal('-9.99'), lang='kmk'),
+            num2words(Decimal('-9.99'), lang='kem'),
             'menus sibe vírgula sibe sibe'
         )
         self.assertEqual(
-            num2words(Decimal('-7.01'), lang='kmk'),
+            num2words(Decimal('-7.01'), lang='kem'),
             'menus itsu vírgula bai sia'
         )
         self.assertEqual(
-            num2words(Decimal('-222.22'), lang='kmk'),
+            num2words(Decimal('-222.22'), lang='kem'),
             'menus atsus rua gulurua resin rua vírgula rua rua'
         )
 
     def test_ordinal(self):
-        self.assertEqual(num2words(1, lang='kmk', ordinal=True), 'saba')
-        self.assertEqual(num2words(2, lang='kmk', ordinal=True), 'abarua')
-        self.assertEqual(num2words(3, lang='kmk', ordinal=True), 'abatelu')
-        self.assertEqual(num2words(4, lang='kmk', ordinal=True), 'abapaat')
-        self.assertEqual(num2words(5, lang='kmk', ordinal=True), 'abalima')
-        self.assertEqual(num2words(6, lang='kmk', ordinal=True), 'abaneem')
-        self.assertEqual(num2words(7, lang='kmk', ordinal=True), 'abaitsu')
-        self.assertEqual(num2words(8, lang='kmk', ordinal=True), 'ababalu')
-        self.assertEqual(num2words(9, lang='kmk', ordinal=True), 'abasibe')
-        self.assertEqual(num2words(10, lang='kmk', ordinal=True), 'abasapulu')
+        self.assertEqual(num2words(1, lang='kem', ordinal=True), 'saba')
+        self.assertEqual(num2words(2, lang='kem', ordinal=True), 'abarua')
+        self.assertEqual(num2words(3, lang='kem', ordinal=True), 'abatelu')
+        self.assertEqual(num2words(4, lang='kem', ordinal=True), 'abapaat')
+        self.assertEqual(num2words(5, lang='kem', ordinal=True), 'abalima')
+        self.assertEqual(num2words(6, lang='kem', ordinal=True), 'abaneem')
+        self.assertEqual(num2words(7, lang='kem', ordinal=True), 'abaitsu')
+        self.assertEqual(num2words(8, lang='kem', ordinal=True), 'ababalu')
+        self.assertEqual(num2words(9, lang='kem', ordinal=True), 'abasibe')
+        self.assertEqual(num2words(10, lang='kem', ordinal=True), 'abasapulu')
         self.assertEqual(
-            num2words(11, lang='kmk', ordinal=True), 'décimu primeiru'
+            num2words(11, lang='kem', ordinal=True), 'décimu primeiru'
         )
         self.assertEqual(
-            num2words(12, lang='kmk', ordinal=True), 'décimu segundu'
+            num2words(12, lang='kem', ordinal=True), 'décimu segundu'
         )
         self.assertEqual(
-            num2words(13, lang='kmk', ordinal=True), 'décimu terceiru'
+            num2words(13, lang='kem', ordinal=True), 'décimu terceiru'
         )
         self.assertEqual(
-            num2words(14, lang='kmk', ordinal=True), 'décimu quartu'
+            num2words(14, lang='kem', ordinal=True), 'décimu quartu'
         )
         self.assertEqual(
-            num2words(15, lang='kmk', ordinal=True), 'décimu quintu'
+            num2words(15, lang='kem', ordinal=True), 'décimu quintu'
         )
         self.assertEqual(
-            num2words(16, lang='kmk', ordinal=True), 'décimu sextu'
+            num2words(16, lang='kem', ordinal=True), 'décimu sextu'
         )
         self.assertEqual(
-            num2words(17, lang='kmk', ordinal=True), 'décimu sétimu'
+            num2words(17, lang='kem', ordinal=True), 'décimu sétimu'
         )
         self.assertEqual(
-            num2words(18, lang='kmk', ordinal=True), 'décimu oitavu'
+            num2words(18, lang='kem', ordinal=True), 'décimu oitavu'
         )
         self.assertEqual(
-            num2words(19, lang='kmk', ordinal=True), 'décimu nonu'
+            num2words(19, lang='kem', ordinal=True), 'décimu nonu'
         )
         self.assertEqual(
-            num2words(20, lang='kmk', ordinal=True), 'vigésimu'
-        )
-
-        self.assertEqual(
-            num2words(21, lang='kmk', ordinal=True), 'vigésimu primeiru'
-        )
-        self.assertEqual(
-            num2words(22, lang='kmk', ordinal=True), 'vigésimu segundu'
-        )
-        self.assertEqual(
-            num2words(35, lang='kmk', ordinal=True), 'trigésimu quintu'
-        )
-        self.assertEqual(
-            num2words(99, lang='kmk', ordinal=True), 'nonagésimu nonu'
+            num2words(20, lang='kem', ordinal=True), 'vigésimu'
         )
 
         self.assertEqual(
-            num2words(100, lang='kmk', ordinal=True), 'centésimu'
+            num2words(21, lang='kem', ordinal=True), 'vigésimu primeiru'
         )
         self.assertEqual(
-            num2words(101, lang='kmk', ordinal=True), 'centésimu primeiru'
+            num2words(22, lang='kem', ordinal=True), 'vigésimu segundu'
         )
         self.assertEqual(
-            num2words(128, lang='kmk', ordinal=True),
+            num2words(35, lang='kem', ordinal=True), 'trigésimu quintu'
+        )
+        self.assertEqual(
+            num2words(99, lang='kem', ordinal=True), 'nonagésimu nonu'
+        )
+
+        self.assertEqual(
+            num2words(100, lang='kem', ordinal=True), 'centésimu'
+        )
+        self.assertEqual(
+            num2words(101, lang='kem', ordinal=True), 'centésimu primeiru'
+        )
+        self.assertEqual(
+            num2words(128, lang='kem', ordinal=True),
             'centésimu vigésimu oitavu'
         )
         self.assertEqual(
-            num2words(713, lang='kmk', ordinal=True),
+            num2words(713, lang='kem', ordinal=True),
             'septigentésimu décimu terceiru'
         )
 
         self.assertEqual(
-            num2words(1000, lang='kmk', ordinal=True), 'milésimu'
+            num2words(1000, lang='kem', ordinal=True), 'milésimu'
         )
         self.assertEqual(
-            num2words(1001, lang='kmk', ordinal=True), 'milésimu primeiru'
+            num2words(1001, lang='kem', ordinal=True), 'milésimu primeiru'
         )
         self.assertEqual(
-            num2words(1111, lang='kmk', ordinal=True),
+            num2words(1111, lang='kem', ordinal=True),
             'milésimu centésimu décimu primeiru'
         )
         self.assertEqual(
-            num2words(2114, lang='kmk', ordinal=True),
+            num2words(2114, lang='kem', ordinal=True),
             'segundu milésimu centésimu décimu quartu'
         )
         self.assertEqual(
-            num2words(73421, lang='kmk', ordinal=True),
+            num2words(73421, lang='kem', ordinal=True),
             'septuagésimu terceiru milésimu quadrigentésimu vigésimu primeiru'
         )
 
         self.assertEqual(
-            num2words(100000, lang='kmk', ordinal=True),
+            num2words(100000, lang='kem', ordinal=True),
             'centésimu milésimu'
         )
         self.assertEqual(
-            num2words(250050, lang='kmk', ordinal=True),
+            num2words(250050, lang='kem', ordinal=True),
             'ducentésimu quinquagésimu milésimu quinquagésimu'
         )
         self.assertEqual(
-            num2words(6000000, lang='kmk', ordinal=True), 'sextu milionésimu'
+            num2words(6000000, lang='kem', ordinal=True), 'sextu milionésimu'
         )
         self.assertEqual(
-            num2words(19000000000, lang='kmk', ordinal=True),
+            num2words(19000000000, lang='kem', ordinal=True),
             'décimu nonu milésimu milionésimu'
         )
         self.assertEqual(
-            num2words(145000000002, lang='kmk', ordinal=True),
+            num2words(145000000002, lang='kem', ordinal=True),
             'centésimu quadragésimu quintu milésimu milionésimu segundu'
         )
 
